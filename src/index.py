238ec47f7b37d2a3da022343296c70885af492ea
Sunsigil <45524115+sunsigil@users.noreply.github.com>
2025-12-01 01:07:51 -0500

    Content update

--- src/index.py
+++ src/index.py
@@ -6,11 +6,10 @@ from html_writer import HTMLWriter, HTMLMode;
 
 def build(here, html: HTMLWriter):
 	html.heading(1, "Cornerlight Home Page");
-	html.heading(3, "This is an internal site. Please do not share it with people outside of Cornerlight.");
 	html.newline();
 
-	html.link("Drive", "https://drive.google.com/drive/u/0/folders/1A6tQIlEG87xxfs52wX-tnvT1XVfzIZtP");
-	html.newline();
+	html.text("For now, this is an internal site. Please do not share it with people outside of Cornerlight.");
+
 	html.link("Interest Form", "https://docs.google.com/forms/d/e/1FAIpQLSeF3szRpMJT9puLkjm_bSyRpTZ3o3xtk9ZjpDnJlCY9T3g6NQ/viewform?usp=header");
 	html.newline();
 	html.link("Volumes", "volumes/index.html");
