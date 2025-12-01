29eb8ff51a257abd16ff822d4e1c77ba0ba44de4
Sunsigil <45524115+sunsigil@users.noreply.github.com>
2025-11-30 22:44:50 -0500

    Added some content

---
+++ src/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# PAGE(Home)
+
+from html_writer import HTMLWriter, HTMLMode;
+
+def build(here, html: HTMLWriter):
+	html.heading(1, "Cornerlight Home Page");
+	html.newline();
+
+	html.link("Drive", "https://drive.google.com/drive/u/0/folders/1A6tQIlEG87xxfs52wX-tnvT1XVfzIZtP");
+	html.newline();
+	html.link("Interest Form", "https://docs.google.com/forms/d/e/1FAIpQLSeF3szRpMJT9puLkjm_bSyRpTZ3o3xtk9ZjpDnJlCY9T3g6NQ/viewform?usp=header");
