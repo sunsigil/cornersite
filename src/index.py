a442b33fb7d1884461f049de1778dd7cf2bec1db
Sunsigil <45524115+sunsigil@users.noreply.github.com>
2026-02-06 22:22:04 -0600

    Update

--- src/index.py
+++ src/index.py
@@ -8,7 +8,7 @@ def build(env, self, html: HTMLWriter):
 
 	html.heading(2, "Announcements");
 	html.text("For now, this is an internal site. Please do not share it with people outside of Cornerlight.");
-	html.text("We are currently working on Volume 6. The theme is \"New Year\".");
+	html.text("We are currently working on volume 6 and accepting page requests for volume 7.");
 
 	html.heading(2, "Links");
 	html.link("Interest Form", "https://docs.google.com/forms/d/e/1FAIpQLSeF3szRpMJT9puLkjm_bSyRpTZ3o3xtk9ZjpDnJlCY9T3g6NQ/viewform?usp=header");
@@ -23,10 +23,11 @@ def build(env, self, html: HTMLWriter):
 	html.heading(2, "FAQ");
 
 	html.heading(3, "Can I contribute to the current Cornerlight issue?");
-	html.text("No. It's already underway.");
-
-	html.heading(3, "Can I contribute to the next Cornerlight issue?");
-	html.text("Yes! Please fill out the interest form linked above.");
+	html.start_text_block();
+	html.text("Yes! Please fill out the page request form outlined above.");
+	html.text("Understand that if you submit a page request, you will be expected to submit your pages by the volume's release date.");
+	html.text("As such, you should not try to get pages in a volume that is a week or less away from release unless you already have the work done.");
+	html.end_text_block();
 
 	html.heading(3, "How should I submit my pages?");
 	html.text("Please upload them via the submission form linked above. One file per page is ideal.");
