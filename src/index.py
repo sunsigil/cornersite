fbc844dc608ed78a936fa7c11d2e2fda9f849342
Sunsigil <45524115+sunsigil@users.noreply.github.com>
2026-02-06 21:35:42 -0600

    Update

--- src/index.py
+++ src/index.py
@@ -3,13 +3,13 @@
 from html_writer import HTMLWriter, HTMLMode;
 
 def build(env, self, html: HTMLWriter):
-	html.heading(1, "Cornerlight Home Page");
-	html.newline();
+    html.heading(1, "Cornerlight Home Page");
+    html.newline();
 
-	html.text("For now, this is an internal site. Please do not share it with people outside of Cornerlight.");
+    html.text("For now, this is an internal site. Please do not share it with people outside of Cornerlight.");
 
-	html.link("Interest Form", "https://docs.google.com/forms/d/e/1FAIpQLSeF3szRpMJT9puLkjm_bSyRpTZ3o3xtk9ZjpDnJlCY9T3g6NQ/viewform?usp=header");
-	html.newline();
-	html.link("Volumes", "volumes/index.html");
-	html.newline();
-	html.link("Itch", "https://sunsigil.itch.io/cornerlight");
+    html.link("Drive", "https://drive.google.com/drive/folders/1A6tQIlEG87xxfs52wX-tnvT1XVfzIZtP?usp=drive_link");
+    html.link("Interest Form", "https://docs.google.com/forms/d/e/1FAIpQLSeF3szRpMJT9puLkjm_bSyRpTZ3o3xtk9ZjpDnJlCY9T3g6NQ/viewform?usp=header");
+    html.link("Submission Form", "https://docs.google.com/forms/d/e/1FAIpQLSeF3szRpMJT9puLkjm_bSyRpTZ3o3xtk9ZjpDnJlCY9T3g6NQ/viewform?usp=header");
+    html.newline();
+    html.link("Itch", "https://sunsigil.itch.io/cornerlight");
